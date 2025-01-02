1d1911f3693a042fd0cf5b10c770c44965a85c8f
Hey there! I'm franck pagnol Nounjo <99295070+Pagnol237@users.noreply.github.com>
2025-01-02 14:03:07 -0600

    update scale
    remove initial scale

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width" />
       </Head>
       <Nav/>
       <Header/>
