ceff773c3ddccc746dc37544c253ef6361553e14
Hey there! I'm franck pagnol Nounjo <99295070+Pagnol237@users.noreply.github.com>
2025-01-02 03:03:58 -0600

    update
    new update

--- app/components/UseScreenWidth.tsx
+++ app/components/UseScreenWidth.tsx
@@ -1,13 +1,22 @@
-'use client'
+'use client';
+
 import React, { useState, useEffect } from "react";
 
 const UseScreenWidth = () => {
-const initialSize:number = window.innerWidth;
-  const [screenWidth, setScreenWidth] = useState(12740);
+  // Initialiser à une valeur par défaut jusqu'à ce que le composant se monte
+  const [screenWidth, setScreenWidth] = useState<number | null>(null);
 
   useEffect(() => {
+    // Vérifier si 'window' est défini avant d'accéder à ses propriétés
     const handleResize = () => setScreenWidth(window.innerWidth);
+
+    // Initialiser la taille de l'écran
+    handleResize();
+
+    // Ajouter l'écouteur pour les changements de taille
     window.addEventListener("resize", handleResize);
+
+    // Nettoyer l'écouteur lors du démontage du composant
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
