91037c4f49495656ba194c01a96c5636363e3ea0
Hey there! I'm franck pagnol Nounjo <99295070+Pagnol237@users.noreply.github.com>
2025-01-06 00:14:28 -0600

    remove
    remove som script

--- app/page.tsx
+++ app/page.tsx
@@ -15,9 +15,6 @@ import Head from "next/head";
 export default function Home() {
   return (
     <div>
-      <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1"/>
-      </Head>
       <Nav/>
       <Header/>
       <About/>
